16906dbbfa0ef114f4b5567ea27417792fcebb28
Venkatesan <49652693+venkatesh2615@users.noreply.github.com>
2021-08-19 23:04:14 +0530

    hai message added

--- PytestDemo/test_Demo1.py
+++ PytestDemo/test_Demo1.py
@@ -3,5 +3,6 @@ import pytest
 
 def test_fistDemo():
 	print("test branch")
+	print('waiting')
 	print("so now we add some lines in testbranch ")
 	print("Hello this is my first demo pytest program")
